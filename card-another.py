e3bf2e9af536109010f509102d68c52e81697ec9
enhenglele <478691878@qq.com>
2020-02-22 01:18:24 +0800

    project card fist submmit

---
+++ card-another.py
@@ -0,0 +1,37 @@
+import random
+card_tuple=('wzt','lb','zgl','gbwz')
+package_list=[]
+
+while 1:
+    num=int(input('''
+'czrnbdgq!
+qxzzl
+1.ck
+2.ckbb
+3.zlbb
+4.lk'
+'''))
+    if num==1:
+        num2=int(input('''
+'qxzckcc'
+'''))
+        for i in range(0,num2):
+            randnum=random.randint(0,3) 
+            package_list.append(card_tuple[randnum])
+            print('ncdl:{}'.format(card_tuple[randnum]))
+    elif num==2:
+        if len(package_list)==0:
+            print('qxzyxfubb')
+        else:
+            for i in package_list:
+                print(i)
+    elif num==3:
+        if len(package_list)==0:
+            print('qxzyxfubb')
+        else:
+            package_list.sort()
+            for i in package_list:
+                print(i)
+    elif num==4:
+        break
+
