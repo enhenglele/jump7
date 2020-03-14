490c2b6f5ac47a1003320cb62fa8e50c653efd42
enhenglele <478691878@qq.com>
2020-03-15 00:45:26 +0800

    just test

---
+++ doc.py
@@ -0,0 +1,11 @@
+def compare(a,b):
+    '''
+    函数功能:
+    比较两个数值的大学
+    '''
+    if int(a)<int(b):
+        print('a小于b')
+    else:
+        print('a不小于b')
+print(compare.__doc__)
+compare(12,7)
---
+++ test.py
@@ -0,0 +1,3 @@
+name=input('input you name:')
+age=input('input you age:')
+print('my name is{},I am {}'.format(name,age))
